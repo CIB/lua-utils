d2a049597141cd073274f51d2a9628c6cd812857
cib <cib123@googlemail.com>
2012-09-26 11:14:39 -0700

    Changed the syntax the documentation tool uses.

--- docgen.lua
+++ docgen.lua
@@ -1,17 +1,33 @@
 -- This module manages parsing special lua comment syntax into documentation metadata,
 -- and generating HTML from this metadata.
 
+--** Foobar
+--* Lorem Ipsum is simply dummy text of the printing and typesetting 
+--  industry. Lorem Ipsum has been the industry's standard dummy text ever 
+--  since the 1500s, when an unknown printer took a galley of type and scrambled it 
+--  to make a type specimen book. It has survived not only five centuries, but also the leap 
+--  into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s 
+--  with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+--  publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
 require "parser_utils"
 require "utils"
 
 local tag_normal = "line"
 local tag_head = "head"
 
---== parse_file
---[c]  parse_file(input_text, output_table)
---=  Parses a lua file, extracting information from comments and putting it into @output_table
---=  input_text:  The text contents of the lua file to parse
---=  output_text: A table to generate the extracted information into
+local comment_symbol_translation =
+{
+    ["@"] = "codeblock",
+    ["?"] = "desc",
+    ["!"] = "param"
+}
+
+--** parse_file
+--*@  parse_file(input_text, output_table)
+--*?  Parses a lua file, extracting information from comments and putting it into @output_table
+--*!  input_text:  The text contents of the lua file to parse
+--*!  output_text: A table to generate the extracted information into
 function parse_file(input_text, output_table)
     -- get a local parser instance
     local parser = clone(parser)
@@ -20,24 +36,36 @@ function parse_file(input_text, output_table)
     -- store some other metadata in the parser temporarily
     parser.current_comment_type = nil -- can be one of nil, "head", "code", "comment"
     
+    local trim2 = function(str)
+        local start, stop, rval = str:find("^%S*(.*)%S*$")
+        return rval
+    end
+    
     while not parser:at_end() do
-        if parser:skip("--= ") then
+        if parser:skip("--* ") then
             -- we got a normal comment
             store_comment_type(parser, output_table)
             parser.current_comment_type = tag_normal
-            parser.current_comment_value = parser:skip_to_next_line()
-        elseif parser:skip("--== ") then
+            parser.current_comment_value = trim(parser:skip_to_next_line())
+        elseif parser:skip("--** ") then
             -- we got a head comment
             store_comment_type(parser, output_table)
             parser.current_comment_type = tag_head
-            parser.current_comment_value = parser:skip_to_next_line()
-        elseif parser:is_next_pattern("--%[(.)%] ") then
+            parser.current_comment_value = trim(parser:skip_to_next_line())
+        elseif parser:is_next_pattern("--%*. ") then
             -- we got a special comment type
-            match, format_char = parser:skip_pattern("--%[(.)%]")
+            match, format_char = parser:skip_pattern("--%*(.) ")
             
             store_comment_type(parser, output_table)
+            -- translate symbols if possible
+            if comment_symbol_translation[format_char] then
+                format_char = comment_symbol_translation[format_char]
+            end
             parser.current_comment_type = format_char
-            parser.current_comment_value = parser:skip_to_next_line()
+            parser.current_comment_value = trim(parser:skip_to_next_line())
+        elseif parser:skip("--") and parser.current_comment_type then
+            -- a continuation of the last comment
+            parser.current_comment_value = parser.current_comment_value .. " " .. trim(parser:skip_to_next_line())
         else
             if parser.current_comment_type then
                 -- store the comment in the table
@@ -49,9 +77,9 @@ function parse_file(input_text, output_table)
     end
 end
 
---= store_comment_type(parser, output_table)
---= Stores the comment the parser has last parsed into the output table,
---  then resets the parser's comment.
+--* store_comment_type(parser, output_table)
+--* Stores the comment the parser has last parsed into the output table,
+--* then resets the parser's comment.
 function store_comment_type(parser, output_table)
     -- if there's no comment, do nothing
     if parser.current_comment_type == nil then return end
@@ -67,9 +95,9 @@ function store_comment_type(parser, output_table)
     parser.current_comment_value = nil
 end
 
---== generate_html
---=  html = generate_html(metadata)
---=  Takes a table with metadata as populated by `parse_file` and generates an HTML document from it.
+--** generate_html
+--*  html = generate_html(metadata)
+--*  Takes a table with metadata as populated by `parse_file` and generates an HTML document from it.
 function generate_html(metadata)
     html = "<html><head><title>Documentation Test</title></head><body>"
     
